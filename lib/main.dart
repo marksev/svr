84542235811dcd443ba930198e131d582ced8495
marksev <33703765+marksev@users.noreply.github.com>
2026-03-06 20:07:06 +0200

    Add GitHub Actions workflow to build Android APK

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+
+import 'game/game_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  CameraController? _controller;
-  bool _isRecording = false;
-
-  Future<void> _toggleRecording() async {
-    if (_controller == null) {
-      final cameras = await availableCameras();
-      _controller = CameraController(cameras.first, ResolutionPreset.medium);
-      await _controller!.initialize();
-    }
-    if (_isRecording) {
-      await _controller!.stopVideoRecording();
-    } else {
-      await _controller!.startVideoRecording();
-    }
-    setState(() {
-      _isRecording = !_isRecording;
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Recorder',
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: _toggleRecording,
-            child: Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GamePage(),
     );
   }
 }
